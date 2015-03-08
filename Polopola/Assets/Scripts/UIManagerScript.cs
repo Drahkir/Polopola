fe904f508c6dc0b14239d84f70a9f259bc217429
drahkir <rikmikac@gmail.com>
2015-03-08 15:10:06 -0400

    Added a replay button in the Game Over scene

---
+++ Polopola/Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManagerScript : MonoBehaviour
+{
+    public Text BestTime;
+
+//    void Start()
+//    {
+//        BestTime.text = "Best Time: " + PlayerPrefs.GetString("BestTime");
+//    }
+
+    public void StartGame()
+    {
+        Application.LoadLevel("Polopola");
+    }
+
+//    public void ResetScore()
+//    {
+//        Grid.ResetBestTime();
+//        BestTime.text = "Best Time: " + PlayerPrefs.GetString("BestTime");
+//    }
+}
